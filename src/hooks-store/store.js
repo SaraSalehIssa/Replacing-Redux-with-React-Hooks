1e81fac794ac413f4cdca48944beed59ceb0b15b
SaraSalehIssa <issasalehsara2001@gmail.com>
2023-09-06 14:33:56 +0300

    Updating

--- src/hooks-store/store.js
+++ src/hooks-store/store.js
@@ -1,17 +1,39 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 let globalState = {};
 let listeners = [];
-let action = {};
+let actions = {};
 
-const useStore = () => {
+export const useStore = (shouldListen = true) => {
     const setState = useState(globalState)[1];
 
+    const dispatch = (actionIdentifier, payload) => {
+        const newState = actions[actionIdentifier](globalState, payload);
+        globalState = { ...globalState, ...newState };
+
+        for (const listener of listeners) {
+            listener(globalState);
+        }
+    };
+
     useEffect(() => {
-        listeners.push(setState);
+        if (shouldListen) {
+            listeners.push(setState);
+        }
 
         return () => {
-            listeners = listeners.filter(li => li !== setState);
-        }
-    }, [setState]);
+            if (shouldListen) {
+                listeners = listeners.filter(li => li !== setState);
+            }
+        };
+    }, [setState, shouldListen]);
+
+    return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+    if (initialState) {
+        globalState = { ...globalState, ...initialState };
+    }
+    actions = { ...actions, ...userActions };
 };
